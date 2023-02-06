87cd70c0162a5594a57e15800b45fb63b5d03397
hidenorioka <vwkr6t@gmail.com>
2023-02-06 23:22:49 +0900

    introduce prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+/** @see https://github.com/withastro/prettier-plugin-astro#pnpm-support-1 */
+
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
